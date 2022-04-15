bb0a3b73f77b18d3702d959d402f98f67e55132b
Aydogan Beyza <bnur933@gmail.com>
2022-04-15 15:53:50 +0300

    연습

--- src/type/PracticeGit.java
+++ src/type/PracticeGit.java
@@ -1,14 +1,25 @@
 package type;
 
+import java.util.ArrayList;
+
 public class PracticeGit {
 
-    //hola
+    public static void main(String[] args) {
+        ArrayList<String> cars = new ArrayList<String>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+        /*for (int i = 0; i < cars.size(); i++) {
+            System.out.println(cars.get(i));
+        }*/
+        for (String car : cars) {
+            System.out.println(car);
+        }
 
-    // 안녕하세요
 
-    //연습하고 있습니다.
 
-    // 힘드네.
+    }
 
 
 }
